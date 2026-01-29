6ec0f91ef09e609115bf88961a9dc9ecae52cc54
Lncvrt <lncvrtreal@gmail.com>
2026-01-29 01:05:43 -0700

    Change project structure and other stuff

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+cfg=Release
+outdir=bin/$cfg
+zipdir=$(pwd)/builds
+rids=(
+  linux-x64 linux-arm linux-arm64 linux-musl-x64 linux-musl-arm64
+  win-x86 win-x64 win-arm64
+  osx-x64 osx-arm64
+)
+
+projfile=$(ls *.csproj | head -1)
+if [[ -z "$projfile" ]]; then
+  echo "No .csproj file found, aborting."
+  exit 1
+fi
+
+version=$(sed -n 's:.*<Version>\(.*\)</Version>.*:\1:p' "$projfile" | head -n1)
+if [[ -z "$version" ]]; then
+  version="0.0.0"
+fi
+
+rm -rf bin
+rm -rf obj
+rm -rf "$zipdir"
+mkdir -p "$zipdir"
+
+name=$(basename "$projfile" .csproj)
+
+for rid in "${rids[@]}"; do
+  dotnet publish "$projfile" -c $cfg -r $rid --self-contained true -o "$outdir/$rid/publish"
+  pushd "$outdir/$rid/publish"
+  7z a -tzip "$zipdir/${name}-${version}-${rid}.zip" ./*
+  popd
+done
